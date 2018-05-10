7b9b5373dbe970afec3e82c6a805eb0ccbe4fa20
domisherdian <36118091+domisherdian@users.noreply.github.com>
2018-05-11 06:17:48 +0700

    Add files via upload

---
+++ daftar-isi-1.js
@@ -0,0 +1,27 @@
+var toc_config = {
+	url: 'https://cetakundangan500.blogspot.com',
+	containerId: 'table-of-content',
+	showNew: 15,
+	newText: ' <strong style="font-weight:normal;font-style:normal;color:#fff;font-size:11px;background:#5c5a78;padding:1px 6px 3px 6px;line-height:normal;float:right;border-radius:3px;">baru</strong>',
+	sortAlphabetically: {
+		thePanel: true,
+		theList: true
+	},
+	maxResults: 9999,
+	activePanel: 1,
+	slideSpeed: {
+		down: 400,
+		up: 400
+	},
+	slideEasing: {
+		down: null,
+		up: null
+	},
+	slideCallback: {
+		down: function() {},
+		up: function() {}
+	},
+	clickCallback: function() {},
+	jsonCallback: '_toc',
+	delayLoading: 0
+};
